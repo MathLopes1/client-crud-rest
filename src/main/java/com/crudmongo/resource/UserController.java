e0af6f6923542dbe91e27519c5f430f1db771f4d
Matheus <matheus.pgm2000@gmail.com>
2022-04-15 18:36:23 -0300

    ref: controller with service

--- src/main/java/com/crudmongo/resource/UserController.java
+++ src/main/java/com/crudmongo/resource/UserController.java
@@ -4,23 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudmongo.domain.User;
+import com.crudmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserController {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1","Maria Brown", "maria@gmail.com");
-		User alex = new User("2","Alex Green", "alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+		
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
