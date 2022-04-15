226120c2e5465856687526a88b6515c07f6034fa
Matheus <matheus.pgm2000@gmail.com>
2022-04-15 18:17:01 -0300

    ref: rename packges

--- src/main/java/com/crudmongo/MongoCrudApplication.java
+++ src/main/java/com/crudmongo/MongoCrudApplication.java
@@ -1,4 +1,4 @@
-package com.matheuslopes.crudmongo;
+package com.crudmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
