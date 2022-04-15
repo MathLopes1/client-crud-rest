748b2c3948a076e68ef488a2969e5bbfa7d6d867
Matheus <matheus.pgm2000@gmail.com>
2022-04-15 18:31:37 -0300

    feat: adding decorators in the user model class

--- src/main/java/com/crudmongo/repository/UserRepository.java
+++ src/main/java/com/crudmongo/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.crudmongo.repository;
 
-public interface UserRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crudmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository <User, String> {
 
 }
--- src/main/java/com/crudmongo/services/UserService.java
+++ src/main/java/com/crudmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.crudmongo.domain.User;
+import com.crudmongo.repository.UserRepository;
 
 @Service
 public class UserService {
@@ -14,6 +15,6 @@ public class UserService {
 	private UserRepository repo;
 	
 	public List<User> findAll(){
-		
+		return repo.findAll();
 	}
 }
