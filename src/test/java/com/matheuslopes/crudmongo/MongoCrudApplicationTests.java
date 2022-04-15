a931bc274451546b7e31c0b9addb3a4df4a6d1ef
Matheus <matheus.pgm2000@gmail.com>
2022-04-15 17:23:13 -0300

    feat: starting spring boot project

---
+++ src/test/java/com/matheuslopes/crudmongo/MongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuslopes.crudmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
