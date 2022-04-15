d2ed44fafca1ae4a17170cd8c94b255c8bb65a3e
Matheus <matheus.pgm2000@gmail.com>
2022-04-15 18:46:47 -0300

    ref: rename controller package

--- src/main/java/com/crudmongo/controller/UserController.java
+++ src/main/java/com/crudmongo/controller/UserController.java
@@ -1,7 +1,5 @@
-package com.crudmongo.resource;
+package com.crudmongo.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
