29975c28d5a4dba62bfd9eac308e9316f0d58b2c
Matheus <matheus.pgm2000@gmail.com>
2022-04-15 19:15:54 -0300

    feat: adding dto pattern to return a list of users

--- src/main/java/com/crudmongo/controller/UserController.java
+++ src/main/java/com/crudmongo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.crudmongo.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudmongo.domain.User;
+import com.crudmongo.dto.UserDTO;
 import com.crudmongo.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserController {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
